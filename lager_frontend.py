73d374459d71ccc78a25e13ee4b27eda1e6c50e8
Kristian Sæberg <kristian@Kristian-sin-MacBook-Air.local>
2023-10-28 09:09:10 +0200

    la til funksjon for å gi oppskrifter som man har nok ingredienser til

--- lager_frontend.py
+++ lager_frontend.py
@@ -1,6 +1,8 @@
 import tkinter
 import customtkinter
 from CTkTable import *
+from tkinter import ttk
+from frontend import *
 
 class RadioButtonFrame(customtkinter.CTkFrame):
     def __init__(self, *args, header_name="RadioButtonFrame", **kwargs):
@@ -10,17 +12,9 @@ class RadioButtonFrame(customtkinter.CTkFrame):
 
         self.header = customtkinter.CTkLabel(self, text=self.header_name)
         self.header.grid(row=0, column=0, padx=10, pady=10)
-        
-        root = customtkinter.CTk()
 
-        value = [[1,2,3,4,5],
-                [1,2,3,4,5],
-                [1,2,3,4,5],
-                [1,2,3,4,5],
-                [1,2,3,4,5]]
-
-        table = CTkTable(master=root, row=5, column=5, values=value)
-        table.pack(expand=True, fill="both", padx=20, pady=20)
+        table = ttk.Treeview(App)
+        table.pack()
 
     def get_value(self):
         """ returns selected value as a string, returns an empty string if nothing selected """
